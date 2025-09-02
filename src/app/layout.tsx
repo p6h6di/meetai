76caa7663dae87f5a76687ec0ec1548991e7bf00
thakur0607 <shivam.thakur@livcheers.com>
2025-09-02 12:14:14 +0530

    Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Meet AI",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn("antialiased", poppins.className)}>{children}</body>
+    </html>
+  );
+}
