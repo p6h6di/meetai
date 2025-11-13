ad6283697c8d8baf3c3dbb1b0a9da9b167b3a4d7
thakur0607 <shivam.thakur@livcheers.com>
2025-11-13 22:43:42 +0530

    Adding trpc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
+import { TRPCReactProvider } from "@/trpc/client";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -19,8 +20,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("antialiased", poppins.className)}>{children}</body>
-    </html>
+    <TRPCReactProvider>
+      <html lang="en">
+        <body className={cn("antialiased", poppins.className)}>{children}</body>
+      </html>
+    </TRPCReactProvider>
   );
 }
