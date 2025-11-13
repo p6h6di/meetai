579675c026323f50471499fe1216a9b320228dea
thakur0607 <shivam.thakur@livcheers.com>
2025-11-13 22:26:18 +0530

    Adding Dashbaord Navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
+import DashboardNavbar from "@/modules/dashboard/ui/components/dashboard-navbar";
 import DashboardSidebar from "@/modules/dashboard/ui/components/dashboard-sidebar";
 
 export default function HomeLayout({
@@ -10,6 +11,7 @@ export default function HomeLayout({
     <SidebarProvider>
       <DashboardSidebar />
       <main className="flex flex-col h-screen w-screen bg-muted">
+        <DashboardNavbar />
         {children}
       </main>
     </SidebarProvider>
--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -8,6 +8,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useSidebar } from "@/components/ui/sidebar";
 import { authClient } from "@/lib/auth-client";
 import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
@@ -15,6 +16,8 @@ import React from "react";
 
 const DashboardUserButton = () => {
   const router = useRouter();
+
+  const { isMobile } = useSidebar();
   const { data, isPending } = authClient.useSession();
 
   const onLogout = () => {
@@ -30,7 +33,7 @@ const DashboardUserButton = () => {
   if (isPending || !data?.user) return null;
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
         {data.user.image ? (
           <Avatar>
             <AvatarImage src={data.user.image} />
@@ -51,7 +54,11 @@ const DashboardUserButton = () => {
         <ChevronDownIcon className="size-4 shrink-0" />
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent align="end" side="right" className="w-72">
+      <DropdownMenuContent
+        align="end"
+        side={isMobile ? "bottom" : "right"}
+        className="w-72"
+      >
         <DropdownMenuLabel>
           <div className="flex flex-col gap-1">
             <span className="font-medium truncate">{data.user.name}</span>
